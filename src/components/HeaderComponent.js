23dc1ecbe7ab715451dffe1417c79fd0ef417270
newsujan <newsujans@gmail.com>
2018-12-24 09:49:48 +0545

    single page applications part 1

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -19,14 +19,14 @@ class Header extends Component {
 
   render(){
     return(
-      <>
+      <div>
         <Navbar dark expand="md">
           <div className="container">
             <NavbarToggler onClick={this.toggleNav} />
             <NavbarBrand className="mr-auto" href="/">
               <img src="assets/images/logo.png" height="30" width="41" alt="Ristorante Con Fusion" />
             </NavbarBrand>
-            <Collapse isNavOpen={this.state.isNavOpen} navbar>
+            <Collapse isOpen={this.state.isNavOpen} navbar>
               <Nav navbar>
                 <NavItem>
                   <NavLink className="nav-link" to="/home" />
@@ -58,7 +58,7 @@ class Header extends Component {
             </div>
           </div>
         </Jumbotron>
-      < />
+      </div>
     );
   }
 
