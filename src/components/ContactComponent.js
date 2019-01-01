158c571e46aac1a6e08da9e37cfd3d0a8d81a56a
newsujan <newsujans@gmail.com>
2019-01-01 23:20:04 +0545

    react redux form validation II

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
 const required = (val) => val && val.length;
-const maxLength = (len) => (val) => !(val) || (val.lengrh <= len);
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
 const isNumber = (val) => !isNaN(Number(val));
 const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
