e904a4630ef54bccc3786847f0258a539bb7065d
newsujan <newsujans@gmail.com>
2019-01-05 11:57:59 +0545

    redux thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,7 +4,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Button,
   Label, Breadcrumb, BreadcrumbItem, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
-
+import {Loading} from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -131,13 +131,25 @@ const minLength = (len) => (val) => val && (val.length >= len);
     }
 
     const DishDetail = (props) => {
-      console.log('DIshdetail Component render invoked');
-
-         const dish = props.dish;
-         if(dish==null){
-            return(<div></div>)
-        }
-
+      if (props.isLoading) {
+        return(
+          <div className="container">
+            <div className="row">
+              <Loading />
+            </div>
+          </div>
+        );
+      }
+      else if(props.errMess){
+        return(
+          <div className="container">
+            <div className="row">
+              <h4>{props.errMess}</h4>
+            </div>
+          </div>
+        );
+      }
+      else if (props.dish !=null)
         return(
           <div className="container">
             <div className="row">
@@ -163,6 +175,10 @@ const minLength = (len) => (val) => val && (val.length >= len);
             </div>
           </div>
         );
+        else
+          return(
+            <div></div>
+          );
     }
 
 
