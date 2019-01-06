f9ee9087268683dbd74682c094bbd0b6adfb2217
newsujan <newsujans@gmail.com>
2019-01-06 08:45:42 +0545

    react redux form revisited

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import {Breadcrumb, BreadcrumbItem, Button, Label, Col, Row } from 'reactstrap';
 import {Link} from 'react-router-dom';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, Form, Errors, actions } from 'react-redux-form';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
 const isNumber = (val) => !isNaN(Number(val));
 const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+
 
 class Contact extends Component {
   constructor(props){
@@ -19,6 +20,7 @@ class Contact extends Component {
   handleSubmit(values){
     console.log("current State is: " + JSON.stringify(values));
     alert("current State is: " + JSON.stringify(values));
+    this.props.resetFeedbackForm();
   }
 
   render(){
@@ -67,7 +69,7 @@ class Contact extends Component {
                 <h3>Send us Your Feedback</h3>
               </div>
               <div className="col-12 col-md-9">
-                <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
                   <Row className="form-group">
                     <Label htmlFor="firstname" md={2}>First Name</Label>
                     <Col md={10}>
@@ -191,7 +193,7 @@ class Contact extends Component {
                       </Button>
                     </Col>
                   </Row>
-                </LocalForm>
+                </Form>
             </div>
             </div>
         </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import Footer from './FooterComponent';
 import {Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import {connect} from 'react-redux';
 import { addComment, fetchDishes } from '../redux/ActionCreators';
+import { actions } from 'react-redux-form';
 
 const mapStateToProps = state => {
     return{
@@ -23,7 +24,8 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => ({
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId,
     rating, author, comment)),
-  fetchDishes: () => {dispatch(fetchDishes())}
+  fetchDishes: () => {dispatch(fetchDishes())},
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
 });
 
 class Main extends Component {
@@ -50,9 +52,9 @@ class Main extends Component {
 
   const DishWithId = ({match}) => {
       return(
-          <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+          <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
             isLoading={this.props.dishes.isLoading}
-            errMess={this.props.disheserrMess}
+            errMess={this.props.dishes.errMess}
             comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
             addComment={this.props.addComment} />
       );
@@ -65,7 +67,7 @@ class Main extends Component {
         <Route path="/home" component={HomePage} />
         <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} /> } />
         <Route path="/menu/:dishId" component={DishWithId} />
-        <Route exact path="/contactus" component={Contact} />
+        <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} /> } />
         <Route exact path="/aboutus" component={() => <About leaders={this.props.leaders} /> } />
         <Redirect to="/home" />
       </Switch>
